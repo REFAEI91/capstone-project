f24ba3f8a108fcd7e69736434b8309f2ac653bfb
M.Refaei Hannan <56527662+REFAEI91@users.noreply.github.com>
2023-05-04 21:36:55 +0200

    Header Component

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-top: 2rem;
+  @media screen and (max-width: 600px) {
+    font-size: 1.8rem;
+  }
+`;
+const Logo = styled.img`
+  position: absolute;
+  top: 2%;
+  left: 3%;
+
+  @media screen and (max-width: 600px) {
+    width: 70px;
+    height: 70px;
+  }
+`;
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <Logo src="/logo.svg" width={100} height={100} alt="logo" />
+      <Title>MediPlan</Title>
+    </HeaderContainer>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import Header from "@/components/Header";
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <Header />
     </div>
   );
 }
