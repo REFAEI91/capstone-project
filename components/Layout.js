34875dafe991f2ebe49a773ea64d3b1e25cd3c70
M.Refaei Hannan <56527662+REFAEI91@users.noreply.github.com>
2023-05-09 12:10:11 +0200

    add Bookmarks Page

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,29 @@
+import Link from "next/link";
 import Header from "./Header";
-
+import Image from "next/image";
+import styled from "styled-components";
 export default function Layout({ children }) {
+  const NavBar = styled.nav`
+    position: fixed;
+    bottom: 1rem;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    padding: 0 1rem;
+    background-color: transparent;
+  `;
   return (
     <>
       <Header />
       {children}
+      <NavBar>
+        <Link href="/">
+          <Image src="/home.svg" width={35} height={35} alt="home" />
+        </Link>
+        <Link href="/Bookmarks">
+          <Image src="/bookmark.svg" width={35} height={35} alt="bookmark" />
+        </Link>
+      </NavBar>
     </>
   );
 }
