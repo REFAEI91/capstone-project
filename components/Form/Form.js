ce3b74293ff18e38772349c7d7d533fd24e2b547
M.Refaei Hannan <56527662+REFAEI91@users.noreply.github.com>
2023-05-10 11:13:49 +0200

    add plan page and form

---
+++ components/Form/Form.js
@@ -0,0 +1,123 @@
+export default function Plan() {
+  return (
+    <form>
+      <fieldset>
+        <legend> Medications plan</legend>
+        <label htmlFor="for">
+          {" "}
+          For :
+          <input type="radio" name="for" value="Me" /> <span>Me</span>{" "}
+        </label>
+        <input type="radio" name="for" defaultValue="Someone else" />{" "}
+        <span>Someone else</span>
+        <br />
+        <fieldset>
+          <label htmlFor="name">
+            {" "}
+            Name :
+            <input type="text" name="name" />
+          </label>
+          <label htmlFor="age">
+            {" "}
+            Age :
+            <input type="number" name="age" />
+          </label>
+          <label htmlFor="gender">
+            {" "}
+            Gender :
+            <select name="gender">
+              <option defaultValue="Male">Male</option>
+              <option defaultValue="Female">Female</option>
+              <option defaultValue="Other">Other</option>
+            </select>
+          </label>
+        </fieldset>
+        <label htmlFor="importance">
+          {" "}
+          Importance :
+          <select name="importance">
+            <option defaultValue="High">High</option>
+            <option defaultValue="Medium">Medium</option>
+            <option defaultValue="Low">Low</option>
+          </select>
+        </label>
+        <label htmlFor="medication_name">
+          {" "}
+          Medication Name :
+          <input type="text" name="medication_name" />
+        </label>
+        <label htmlFor="activ_ingredients">
+          {" "}
+          Activ Ingredients :
+          <input type="text" name="activ_ingredients" />
+        </label>
+        <label htmlFor="dosage">
+          {" "}
+          Dosage :
+          <input type="text" name="dosage" />
+        </label>
+        <label htmlFor="reason">
+          {" "}
+          Reason for taking:
+          <input type="text" name="reason" />
+        </label>
+        <label htmlFor="form">
+          {" "}
+          Form :
+          <select name="form">
+            <option defaultValue="Tablet">Tablet</option>
+            <option defaultValue="Capsule">Capsule</option>
+            <option defaultValue="Syrup">Syrup</option>
+            <option defaultValue="Injection">Injection</option>
+            <option defaultValue="Powder">Powder</option>
+            <option defaultValue="Ointment">Ointment</option>
+            <option defaultValue="Syringe">Syringe</option>
+            <option defaultValue="Cream">Cream</option>
+            <option defaultValue="Drops">Drops</option>
+            <option defaultValue="Spray">Spray</option>
+            <option defaultValue="Other">Other</option>
+          </select>
+        </label>
+        <fieldset>
+          <label htmlFor="frequency">
+            {" "}
+            Frequency :
+            <select name="frequencyType">
+              <option defaultValue="Daily">Daily</option>
+              <option defaultValue="Weekly">Weekly</option>
+              <option defaultValue="Monthly">Monthly</option>
+            </select>
+            <select name="frequencyTimes">
+              <option defaultValue="Once">Once</option>
+              <option defaultValue="Twice">Twice</option>
+              <option defaultValue="Thrice">Thrice</option>
+              <option defaultValue="Four times">Four times</option>
+              <option defaultValue="Five times">Five times</option>
+              <option defaultValue="Six times">Six times</option>
+              <option defaultValue="More">More</option>
+            </select>
+          </label>
+          <input type="checkbox" name="frequencyMornings" value="Mornings" />
+          Mornings
+          <input type="checkbox" name="frequencyAfternoon" value="Afternoon" />
+          Afternoon
+          <input type="checkbox" name="frequencyEvening" value="Evening" />
+          Evening
+          <input type="checkbox" name="frequencyBedtime" value="Bedtime" />
+          Bedtime
+        </fieldset>
+        <label htmlFor="instractions">
+          {" "}
+          Instructions :
+          <textarea type="text" name="instractions" />
+        </label>
+        <label htmlFor="refill">
+          {" "}
+          Refill :
+          <input type="date" name="refill" />
+        </label>
+        <button type="submit">Submit</button>
+      </fieldset>
+    </form>
+  );
+}
---
+++ pages/plan.js
@@ -0,0 +1,9 @@
+import Form from "../components/Form/Form";
+
+export default function Plan() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
