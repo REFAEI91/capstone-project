fc5bc9459e35fc187877309ecd9af2d3d49ce6d4
M.Refaei Hannan <56527662+REFAEI91@users.noreply.github.com>
2023-05-12 00:58:26 +0200

    fix Layout style

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,23 +2,49 @@ import Link from "next/link";
 import Header from "../Header/Header";
 import Image from "next/image";
 import { NavBar } from "../Layout/Layout.styled";
+import { useState } from "react";
 export default function Layout({ children }) {
+  const [activeLink, setActiveLink] = useState("");
   return (
     <>
       <Header />
       {children}
       <NavBar>
-        <Link href="/">
-          <Image src="/home.svg" width={35} height={35} alt="home" />
+        <Link href="/" onClick={() => setActiveLink("home")}>
+          <Image
+            src="/home.svg"
+            width={35}
+            height={35}
+            alt="home"
+            className={activeLink === "home" ? "active" : ""}
+          />
         </Link>
-        <Link href="/Bookmarks">
-          <Image src="/bookmark.svg" width={35} height={35} alt="bookmark" />
+        <Link href="/Bookmarks" onClick={() => setActiveLink("bookmarks")}>
+          <Image
+            src="/bookmark.svg"
+            width={35}
+            height={35}
+            alt="bookmark"
+            className={activeLink === "bookmarks" ? "active" : ""}
+          />
         </Link>
-        <Link href="/plan">
-          <Image src="/plan.svg" width={35} height={35} alt="bookmark" />
+        <Link href="/plan" onClick={() => setActiveLink("plan")}>
+          <Image
+            src="/plan.svg"
+            width={35}
+            height={35}
+            alt="plan"
+            className={activeLink === "plan" ? "active" : ""}
+          />
         </Link>
-        <Link href="/profile">
-          <Image src="/profile.svg" width={35} height={35} alt="bookmark" />
+        <Link href="/profile" onClick={() => setActiveLink("profile")}>
+          <Image
+            src="/profile.svg"
+            width={35}
+            height={35}
+            alt="profile"
+            className={activeLink === "profile" ? "active" : ""}
+          />
         </Link>
       </NavBar>
     </>
