48efbfef4d74e265ac7a902865dbb8661b61c1d6
M.Refaei Hannan <56527662+REFAEI91@users.noreply.github.com>
2023-05-22 15:39:41 +0200

    fix

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -4,22 +4,19 @@ import { Button } from "./Login.styled";
 
 export default function Login() {
   const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
+  return (
+    <>
+      {session ? (
         <Button onClick={() => signOut()}>
           <Image src="/logout.svg" width={32} height={32} alt="logout" />
           Sign out
         </Button>
-      </>
-    );
-  }
-  return (
-    <>
-      <Button onClick={() => signIn()}>
-        <Image src="/login.svg" width={32} height={32} alt="login" />
-        Sign in
-      </Button>
+      ) : (
+        <Button onClick={() => signIn()}>
+          <Image src="/login.svg" width={32} height={32} alt="login" />
+          Sign in
+        </Button>
+      )}
     </>
   );
 }
